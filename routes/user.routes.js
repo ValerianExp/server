b4ebc1bfcba32c5baad0c651cb5cf9a9367cd7f8
RedValerian <r.pino.camacho@gmail.com>
2022-09-26 19:32:37 +0200

    User controller and user routes

---
+++ routes/user.routes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+const {
+    getUser,
+    editUser,
+    deleteUser
+} = require('../controller/user.controller');
+
+router.get('/:id', getUser);
+router.put('/:id', editUser);
+router.delete('/:id', deleteUser);
